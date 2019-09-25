71918ba3706207c94db31711425944c7de4a39db
Aluno <aluno@etec.sp.gov.br>
2019-09-25 14:32:19 -0300

    add

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -3,7 +3,11 @@
 <li> <a href="cadastro_alunos.php">Novo Aluno</a></li>
 <li> <a href="lista_alunos.php">Lista de Alunos</a> </li>
 
-<li> <a href="cadastro_curso.php">Lista de Alunos</a> </li>
+<li> <a href="cadastro_curso.php">Novo Curso</a> </li>
 <li> <a href="lista_cursos.php">Lista de Alunos</a> </li>
+
+<li> <a href="cadastro_matricula.php">Nova Matricula</a> </li>
+<li> <a href="lista_cursos.php">Lista de Matriculas</a> </li>
+
 </ul>
 </header>
